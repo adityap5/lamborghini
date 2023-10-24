6ebdb6f7c5661e7ef8fb80a30b6945b89b8636d0
adityap5 <aditya29pippal@gmail.com>
2023-10-25 00:35:36 +0530

    done

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// Shery.imageEffect("#bottom",{style:6 ,
-//     config:{"noiseDetail":{"value":7.44,"range":[0,100]},
-//     "distortionAmount":{"value":2.98,"range":[0,10]},
-//     "scale":{"value":36.36,"range":[0,100]},
-//     "speed":{"value":0.79,"range":[0,1]},
-//     "zindex":{"value":-9996999,"range":[-9999999,9999999]},
-//     "aspect":{"value":2.068965449202786},
-//     "gooey":{"value":true},
-//     "infiniteGooey":{"value":true},
-//     "growSize":{"value":2.39,"range":[1,15]},
-//     "durationOut":{"value":1,"range":[0.1,5]},
-//     "durationIn":{"value":1.5,"range":[0.1,5]},
-//     "displaceAmount":{"value":0.5},
-//     "masker":{"value":false},
-//     "maskVal":{"value":1,"range":[1,5]},
-//     "scrollType":{"value":0},
-//     "geoVertex":{"range":[1,64],"value":1},
-//     "noEffectGooey":{"value":true},
-//     "onMouse":{"value":1},
-//     "noise_speed":{"value":1.16,"range":[0,10]},
-//     "metaball":{"value":0.2,"range":[0,2],
-//     "_gsap":{"id":3}},
-//     "discard_threshold":{"value":0.5,"range":[0,1]},
-//     "antialias_threshold":{"value":0,"range":[0,0.1]},
-//     "noise_height":{"value":0.31,"range":[0,2]},
-//     "noise_scale":{"value":5.79,"range":[0,100]}},
-//     gooey:true})
+Shery.imageEffect("#bottom",{style:6 ,
+    config:{"noiseDetail":{"value":7.44,"range":[0,100]},
+    "distortionAmount":{"value":2.98,"range":[0,10]},
+    "scale":{"value":36.36,"range":[0,100]},
+    "speed":{"value":0.79,"range":[0,1]},
+    "zindex":{"value":-9996999,"range":[-9999999,9999999]},
+    "aspect":{"value":2.068965449202786},
+    "gooey":{"value":true},
+    "infiniteGooey":{"value":true},
+    "growSize":{"value":2.39,"range":[1,15]},
+    "durationOut":{"value":1,"range":[0.1,5]},
+    "durationIn":{"value":1.5,"range":[0.1,5]},
+    "displaceAmount":{"value":0.5},
+    "masker":{"value":false},
+    "maskVal":{"value":1,"range":[1,5]},
+    "scrollType":{"value":0},
+    "geoVertex":{"range":[1,64],"value":1},
+    "noEffectGooey":{"value":true},
+    "onMouse":{"value":1},
+    "noise_speed":{"value":1.16,"range":[0,10]},
+    "metaball":{"value":0.2,"range":[0,2],
+    "_gsap":{"id":3}},
+    "discard_threshold":{"value":0.5,"range":[0,1]},
+    "antialias_threshold":{"value":0,"range":[0,0.1]},
+    "noise_height":{"value":0.31,"range":[0,2]},
+    "noise_scale":{"value":5.79,"range":[0,100]}},
+    gooey:true})
 
-// var elems = document.querySelectorAll(".elem");
-// elems.forEach(function(elem) {
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function(elem) {
     
-// })
+})
 var elems = document.querySelectorAll("#elem");
 elems.forEach(function(elem) {
     var h1s = elem.querySelectorAll("h1");
